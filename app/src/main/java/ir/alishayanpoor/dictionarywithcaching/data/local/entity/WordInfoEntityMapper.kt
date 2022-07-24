e5475f4dd881e1f40164ea9eebf3cb39f2bc27c9
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-24 13:20:03 +0430

    Create WordInfoEntityMapper.kt

---
+++ app/src/main/java/ir/alishayanpoor/dictionarywithcaching/data/local/entity/WordInfoEntityMapper.kt
@@ -0,0 +1,12 @@
+package ir.alishayanpoor.dictionarywithcaching.data.local.entity
+
+import ir.alishayanpoor.dictionarywithcaching.domain.model.WordInfo
+
+fun WordInfoEntity.toWordInfo(): WordInfo {
+    return WordInfo(
+        meanings = meanings,
+        word = word,
+        origin = origin,
+        phonetic = phonetic
+    )
+}
