e9d09b3023e63d48839aa3d7a92fcd15401ab99e
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-24 12:24:21 +0430

    Change table name

--- app/src/main/java/ir/alishayanpoor/dictionarywithcaching/data/local/entity/WordInfoEntity.kt
+++ app/src/main/java/ir/alishayanpoor/dictionarywithcaching/data/local/entity/WordInfoEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import ir.alishayanpoor.dictionarywithcaching.domain.model.Meaning
 
-@Entity
+@Entity(tableName = "word_info_table")
 data class WordInfoEntity(
     val word: String,
     val phonetic: String,
