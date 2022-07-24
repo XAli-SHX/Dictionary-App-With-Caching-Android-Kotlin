619e90e777e78170cbee973a4751408342734886
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-24 12:06:50 +0430

    Add getWordInfo API

--- app/src/main/java/ir/alishayanpoor/dictionarywithcaching/data/remote/DictionaryApi.kt
+++ app/src/main/java/ir/alishayanpoor/dictionarywithcaching/data/remote/DictionaryApi.kt
@@ -1,5 +1,13 @@
 package ir.alishayanpoor.dictionarywithcaching.data.remote
 
+import ir.alishayanpoor.dictionarywithcaching.data.remote.dto.WordInfoDto
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface DictionaryApi {
-    
+    @GET("{word}")
+    suspend fun getWordInfo(
+        @Path("word") word: String,
+    ): Response<WordInfoDto>
 }
