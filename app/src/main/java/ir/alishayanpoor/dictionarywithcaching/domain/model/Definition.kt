8dfbab653ebd6a1fccbdc4f7ae00b106ae1cb699
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-24 12:12:54 +0430

    Create Definition.kt

---
+++ app/src/main/java/ir/alishayanpoor/dictionarywithcaching/domain/model/Definition.kt
@@ -0,0 +1,8 @@
+package ir.alishayanpoor.dictionarywithcaching.domain.model
+
+data class Definition(
+    val definition: String,
+    val synonyms: List<String>,
+    val antonyms: List<String>,
+    val example: String,
+)
